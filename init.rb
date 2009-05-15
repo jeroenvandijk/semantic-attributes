253a60c8ef2fa8add5a1e49a90c4a9dd7d4affb4
Jeroen van Dijk <jeroen@noxa.nl>
2009-05-15 17:28:26 +0200

    Added translations for Dutch and changed the init file so that multiple locales can be loaded

--- init.rb
+++ init.rb
@@ -6,4 +6,4 @@ ActiveRecord::Base.class_eval do
   include ActiveRecord::ValidationRecursionControl
 end
 
-I18n.load_path << File.dirname(__FILE__) + '/lib/semantic_attributes/locale/en.yml'
+I18n.load_path << Dir[File.join(File.dirname(__FILE__) + '/lib/semantic_attributes/locale/', '*.{rb,yml}')]
